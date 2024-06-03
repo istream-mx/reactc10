70b1b22b7cf7044cf5d5cb74a82efd80efa21882
jorge perez <japmedina97@gmail.com>
2024-06-03 15:39:47 -0500

    se agregan cambios enviroments variables

--- src/environments/index.gxs-qa.js
+++ src/environments/index.gxs-qa.js
@@ -1,8 +1,9 @@
 module.exports = {
-  ENV: 'development',
+  ENV: 'c10-qa',
+  APP_NAME: 'Canal 10',
   API_URL: 'https://pulse.canal10mx.com',
   API_KEY: '',
   CONSUMER_KEY: 'HoC5TCPouXAX1Q6dQ7jV4Avl3ewDLjuHVW-GhERkQUA',
-  SENTRY_DSN: '',
-  TENANT: 'c10',
+  IOS_APP_ID: 1483684459,
+  GOOGLE_OAUTH_APP_GUID: '175728106787-vj60v6bolmpvs0828m79mb1869leti1h',
 };

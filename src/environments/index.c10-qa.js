42a6919cd2e43bcf4e9bfefa1d640fb6c68e2398
jorgeperez777 <jorgearmandoperezmedina@gmail.com>
2024-08-27 13:56:16 -0500

    se agrega url para compartir notas en los enviroments

--- src/environments/index.c10-qa.js
+++ src/environments/index.c10-qa.js
@@ -6,4 +6,5 @@ module.exports = {
   CONSUMER_KEY: 'HoC5TCPouXAX1Q6dQ7jV4Avl3ewDLjuHVW-GhERkQUA',
   IOS_APP_ID: 1483684459,
   GOOGLE_OAUTH_APP_GUID: '175728106787-vj60v6bolmpvs0828m79mb1869leti1h',
+  URL_SHARE_NOTES: 'https://noticias.canal10.tv/nota/'
 };

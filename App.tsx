b1c296b15d2311a53d87996de2319a4ba1d4a635
jorgeperez777 <jorgearmandoperezmedina@gmail.com>
2024-10-08 17:06:59 -0500

    se actualiza version de react

--- App.tsx
+++ App.tsx
@@ -12,14 +12,14 @@ import {store, persistor} from './src/app/store';
 import {PersistGate} from 'redux-persist/integration/react';
 import {GluestackUIProvider} from '@gluestack-ui/themed';
 import MainStack from './src/navigation/MainStack';
-import SplashScreen from 'react-native-splash-screen';
-import {Platform} from 'react-native';
+// import SplashScreen from 'react-native-splash-screen';
+// import {Platform} from 'react-native';
 const App = () => {
-  React.useEffect(() => {
-    if (Platform.OS === 'android') {
-      SplashScreen.hide();
-    }
-  }, []);
+  // React.useEffect(() => {
+  //   if (Platform.OS === 'android') {
+  //     SplashScreen.hide();
+  //   }
+  // }, []);
 
   return (
     <Provider store={store}>
